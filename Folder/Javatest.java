c3429369ff3b264735d4158ec849953ed4180303
GusBraganca <gusbraganca@gmail.com>
2022-02-05 04:20:55 +1100

    commit

--- Folder/Javatest.java
+++ Folder/Javatest.java
@@ -1,9 +1,10 @@
 class HelloWorld {
     public static void main (String[] args){
 
-        System.out.println("Hello World");
-        System.out.println("segunda linha");
-        System.out.println("terceira linha");
+        int cont = 0;
+
+        System.out.println("Vc digitou " + cont);
+    
     }
 
 }
